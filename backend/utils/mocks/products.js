a35b160a2f505b7011a6d9dcc78993cc21215471
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-06-19 00:46:25 -0500

    se agregaron mocks de categories, subcategories y products

---
+++ backend/utils/mocks/products.js
@@ -0,0 +1,155 @@
+const products = [
+  {
+    Id_Product: 1,
+    Title: 'Coffee Caramel Biscotti',
+    Description:
+      'risus praesent lectus vestibulum quam sapien varius ut blandit non interdum in ante vestibulum ante ipsum primis in faucibus',
+    Price: '$17.11',
+    PromotionPrice: '$7.90',
+    Stock: 84,
+  },
+  {
+    Id_Product: 2,
+    Title: 'Island Oasis - Banana Daiquiri',
+    Description:
+      'curabitur convallis duis consequat dui nec nisi volutpat eleifend donec ut dolor morbi vel lectus',
+    Price: '$12.57',
+    PromotionPrice: '$10.85',
+    Stock: 79,
+  },
+  {
+    Id_Product: 3,
+    Title: 'Chicken - Tenderloin',
+    Description:
+      'donec dapibus duis at velit eu est congue elementum in hac habitasse platea',
+    Price: '$14.54',
+    PromotionPrice: '$14.89',
+    Stock: 96,
+  },
+  {
+    Id_Product: 4,
+    Title: 'Propel Sport Drink',
+    Description:
+      'dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula',
+    Price: '$14.82',
+    PromotionPrice: '$12.52',
+    Stock: 96,
+  },
+  {
+    Id_Product: 5,
+    Title: 'Cardamon Ground',
+    Description: 'tellus in sagittis dui vel nisl duis ac nibh fusce',
+    Price: '$4.29',
+    PromotionPrice: '$4.81',
+    Stock: 46,
+  },
+  {
+    Id_Product: 6,
+    Title: 'Bread - Ciabatta Buns',
+    Description:
+      'diam erat fermentum justo nec condimentum neque sapien placerat ante nulla justo aliquam quis turpis eget elit sodales',
+    Price: '$8.60',
+    PromotionPrice: '$9.45',
+    Stock: 53,
+  },
+  {
+    Id_Product: 7,
+    Title: 'Sponge Cake Mix - Chocolate',
+    Description:
+      'sed interdum venenatis turpis enim blandit mi in porttitor pede justo eu massa donec dapibus duis at velit eu est',
+    Price: '$16.61',
+    PromotionPrice: '$13.25',
+    Stock: 37,
+  },
+  {
+    Id_Product: 8,
+    Title: 'Cheese - Goat With Herbs',
+    Description:
+      'etiam pretium iaculis justo in hac habitasse platea dictumst etiam faucibus cursus urna ut',
+    Price: '$8.90',
+    PromotionPrice: '$3.73',
+    Stock: 60,
+  },
+  {
+    Id_Product: 9,
+    Title: 'Beer - Moosehead',
+    Description:
+      'laoreet ut rhoncus aliquet pulvinar sed nisl nunc rhoncus dui vel sem sed sagittis nam congue risus semper porta',
+    Price: '$15.58',
+    PromotionPrice: '$5.89',
+    Stock: 68,
+  },
+  {
+    Id_Product: 10,
+    Title: 'Bread - Frozen Basket Variety',
+    Description: 'nec sem duis aliquam convallis nunc proin at turpis a pede',
+    Price: '$6.18',
+    PromotionPrice: '$7.89',
+    Stock: 21,
+  },
+  {
+    Id_Product: 11,
+    Title: 'Appetizer - Sausage Rolls',
+    Description:
+      'nibh fusce lacus purus aliquet at feugiat non pretium quis lectus suspendisse potenti in eleifend quam a odio in hac',
+    Price: '$19.79',
+    PromotionPrice: '$11.86',
+    Stock: 52,
+  },
+  {
+    Id_Product: 12,
+    Title: 'Wine - Sauvignon Blanc',
+    Description:
+      'pellentesque viverra pede ac diam cras pellentesque volutpat dui maecenas tristique',
+    Price: '$19.08',
+    PromotionPrice: '$13.22',
+    Stock: 77,
+  },
+  {
+    Id_Product: 13,
+    Title: 'Southern Comfort',
+    Description: 'consequat morbi a ipsum integer a nibh in quis justo',
+    Price: '$11.61',
+    PromotionPrice: '$3.31',
+    Stock: 18,
+  },
+  {
+    Id_Product: 14,
+    Title: 'Beef - Texas Style Burger',
+    Description:
+      'duis bibendum felis sed interdum venenatis turpis enim blandit mi in porttitor pede justo eu',
+    Price: '$14.34',
+    PromotionPrice: '$6.47',
+    Stock: 17,
+  },
+  {
+    Id_Product: 15,
+    Title: 'Hickory Smoke, Liquid',
+    Description:
+      'congue etiam justo etiam pretium iaculis justo in hac habitasse platea dictumst etiam',
+    Price: '$9.29',
+    PromotionPrice: '$10.68',
+    Stock: 27,
+  },
+  {
+    Id_Product: 16,
+    Title: 'Pork - Bones',
+    Description:
+      'hendrerit at vulputate vitae nisl aenean lectus pellentesque eget nunc donec',
+    Price: '$11.85',
+    PromotionPrice: '$4.10',
+    Stock: 69,
+  },
+  {
+    Id_Product: 17,
+    Title: 'Tobasco Sauce',
+    Description:
+      'dictumst morbi vestibulum velit id pretium iaculis diam erat fermentum justo nec condimentum neque',
+    Price: '$9.96',
+    PromotionPrice: '$10.61',
+    Stock: 14,
+  }
+];
+module.exports = {
+ products
+}
