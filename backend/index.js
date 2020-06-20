abd55d35d452b957538ef5c04eb31a764084305b
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-06-20 16:49:32 -0500

    se agrego documentacion de categorias y subcategorias

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { config } = require('./config/index');
 
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerDoc = require('./swagger.json');
 const categoriesApi = require('./api/components/categories/network')
 const subcategoriesApi = require('./api/components/subcategories/network');
 
@@ -9,6 +12,9 @@ const app = express();
 
 app.use(bodyParser.json()); // Para datos tipo application/json
 
+
+//Route documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 //Routes
 categoriesApi(app);
 subcategoriesApi(app);
