508bc5548641c449b66ec9b9a50a8929855f6abe
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-06-20 19:25:55 -0500

    se agrego el controller y el index para el crud de subcategories

--- backend/api/components/subcategories/network.js
+++ backend/api/components/subcategories/network.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const subcategoriesMock = require('../../../utils/mocks/subcategories')
-
+const Controller = require('./index');
 function subcategoriesApi(app) {
     const router = express.Router();
 
@@ -8,7 +7,7 @@ function subcategoriesApi(app) {
 
     router.get('/', async function(req, res, next){
         try {
-            const subcategories =  await Promise.resolve(subcategoriesMock);
+            const subcategories =  await Controller.getAll();
             res.status(200).json({
                 data: subcategories,
                 message: 'subcategories listed'
@@ -18,12 +17,12 @@ function subcategoriesApi(app) {
         }
     });
 
-    router.get('/:Id_Category', async function(req, res, next){
+    router.get('/:Id_Subcategory', async function(req, res, next){
         try {
-            const subcategories = await Promise.resolve(subcategoriesMock[req.params.Id_Category]);
+            const subcategories = await Controller.findId(req.params.Id_Subcategory);
             res.status(200).json({
                 data: subcategories,
-                message: 'category retrieve'
+                message: 'subcategory retrieve'
             })
         } catch (error) {
             next(error);
@@ -32,33 +31,33 @@ function subcategoriesApi(app) {
 
     router.post('/', async function(req, res, next){
         try {
-            const createIdCategory =  await Promise.resolve(subcategoriesMock[0].Id_Category);
+            const createIdCategory =  await Controller.upsert(req.body);
             res.status(200).json({
                 data: createIdCategory,
-                message: 'category created'
+                message: 'subcategory created'
             })
         } catch (error) {
             next(error);
         }
     });
-    router.put('/:Id_Category', async function(req, res, next){
+    router.put('/:Id_Subcategory', async function(req, res, next){
         try {
-            const updateIdCategory =  await Promise.resolve(subcategoriesMock[req.params.Id_Category].Id_Category);
+            const updateIdCategory =  await Controller.upsert({...req.body,Id_Subcategory: req.params.Id_Subcategory})
             res.status(200).json({
                 data: updateIdCategory,
-                message: 'category updated'
+                message: 'subcategory updated'
             })
         } catch (error) {
             next(error);
         }
     });
 
-    router.delete('/:Id_Category', async function(req, res, next){
+    router.delete('/:Id_Subcategory', async function(req, res, next){
         try {
-            const deletedIdCategory =  await Promise.resolve(subcategoriesMock[req.params.Id_Category].Id_Category);
+            const deletedIdCategory =  await Controller.remove(req.params.Id_Subcategory);
             res.status(200).json({
                 data: deletedIdCategory,
-                message: 'subcategories deleted'
+                message: 'subcategory deleted'
             })
         } catch (error) {
             next(error);
